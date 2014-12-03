75b3e82355325a99aac82c561e4e7bc8e5757793
Hypolymer <jonesw@geneseo.edu>
2014-12-02 23:10:50 -0500

    update

---
+++ bibSearch.php
@@ -0,0 +1,33 @@
+<?php	
+	require_once('vendor/autoload.php');
+
+	use OCLC\Auth\WSKey;
+	use OCLC\Auth\AccessToken;
+	use WorldCat\Discovery\Bib;
+
+	$filename = "appConfig.ini";
+	$ini_array = parse_ini_file ($filename);
+	
+	$key = $ini_array["key"];
+	$secret = $ini_array["secret"];
+	$institution_id = $ini_array["institution_id"];
+	$options = array('services' => array('WorldCatDiscoveryAPI', 'refresh_token'));
+	$wskey = new WSKey($key, $secret, $options);
+	$accessToken = $wskey->getAccessTokenWithClientCredentials($institution_id, $institution_id);
+	
+	$bib = "";
+	$query = 'name:hunger+games';
+
+$results = Bib::Search($query, $accessToken);
+if (is_a($bib, 'WorldCat\Discovery\Error')) {
+   echo $results->getErrorCode();
+   echo $results->getErrorMessage();
+} else {
+   foreach ($results->getSearchResults() as $bib){
+      echo $bib->getName()->getValue();
+      echo ($bib->getDatePublished() ?  ' ' . $bib->getDatePublished()->getValue()  : '');
+	  echo "<br/><br/>";
+	  ;
+   }
+   }
+?>
