d7439aa65d7443eb861eefaceacd5c792d617f44
SteelsenS <steelsens@gmail.com>
2014-12-05 09:58:07 -0500

    Alternates now appear

---
+++ js/alternate.js
@@ -0,0 +1,24 @@
+function updateAlt() {
+    $("#alternatePane").empty();
+    var url = "getAlternate.php?num=" + document.getElementById("ISSN").value;
+
+    $.getJSON(url, function (data) {
+        var html = "";
+        var altData = data;
+        $.each(data, function (index, value) {
+            html += "<li>";
+            html += [
+                '<a href="http://www.worldcat.org/oclc/'
+            ] + value['oclcn'] + ['">'];
+            html += value['title'] + " ";
+            html += value['author'] + " ";
+            html += value['format'];
+            html += value['language'];
+            html += value['edition'];
+            html += "</a>";
+            html += "</li>";
+        });
+        //$('#alternatePane').html("<h3>It changed</h3>");
+        $('#alternatePane').html("<ul>" + html + "</ul>");
+    });
+}
--- returnError.php
+++ returnError.php
@@ -11,14 +11,3 @@
     }
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title></title>
-    </head>
-    <body>
-        
-    </body>
-</html>
